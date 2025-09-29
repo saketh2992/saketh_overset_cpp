d5eaec3e1b4a9d01add702580d27f81a12654aeb
saketh2992 <saketh2992@gmail.com>
2025-09-29 11:41:39 +0530

    Refactor: reorganize into src/{mesh,solver,util,io}; update includes and CMake; build+run validated

---
+++ src/io/output.h
@@ -0,0 +1,10 @@
+#ifndef OUTPUT_H
+#define OUTPUT_H
+
+#include <string>
+#include "mesh/datastructure.h"
+
+// Output functions
+void GetOutput(DataStructure *rect, std::string input);
+
+#endif // OUTPUT_H
---
+++ src/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include "util/constants.h"
+#include "util/utilities.h"
+#include "mesh/adt.h"
+#include "mesh/datastructure.h"
+#include "solver/solver.h"
+#include "io/output.h"
+
+using namespace std;
+int main() {
+    const int mulFac = 5;
+    DataStructure bgMesh(0.0, 0.0, 0.0 * PI / 180.0 , 1.0, 1.0, 10*mulFac, 10*mulFac);
+    DataStructure compMesh(0.42, 0.42, 0.0 * PI / 180.0, 0.4, 0.4, 6*mulFac, 6*mulFac);
+
+    compMesh.MarkBoundaryDonor(bgMesh);
+    // bgMesh.HoleCutting(compMesh);
+    bgMesh.MarkBoundaryPointType(BC_SPECIFIED);
+    compMesh.MarkBoundaryPointType(INTERPOLATION_RECIEVER);
+    
+    bgMesh.StoreInternalInterpolatedPoints();
+    compMesh.StoreInternalInterpolatedPoints();
+
+    bgMesh.WriteTxtPointType("bgPtType.txt");
+    compMesh.WriteTxtPointType("compPtType.txt");
+
+    initialize(&bgMesh, &compMesh);
+    initialize(&compMesh, &bgMesh);
+    Solve(&bgMesh, &compMesh);
+    GetOutput(&bgMesh, "output_Upwind_bgMesh.dat");
+    GetOutput(&compMesh, "output_Upwind_compMesh.dat");
+    cout << "Solved overset mesh." << endl;
+
+    return 0;
+}
